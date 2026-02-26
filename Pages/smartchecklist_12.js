8717b6be754bb87953328442bb03e3b61040377a
mohammed hashir kp <hashirthalassery98@gmail.com>
2026-02-26 15:17:40 +0530

    Latest Playwright automation update

--- Pages/smartchecklist_12.js
+++ Pages/smartchecklist_12.js
@@ -20,6 +20,109 @@ class smartchecklist_12 {
     this.OutsidereadingOptionClick = page.locator(
       '//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/div/div[3]/div/button[2]/span[2]',
     );
+
+    this.ClickonOPS_CDU_0057 = page.locator(
+      '//*[@id="root"]/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[7]/td[2]/div[1]',
+    );
+
+    this.ClickonReportFinding = page.locator(
+      '//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div/div[3]/div[2]/table/tbody/tr[1]/td[2]/button/svg/path[1]',
+    );
+
+    this.ClickonDropdown = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div[2]/div/div[3]/div/select',
+    );
+
+    this.CreateNewFindngs = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/button',
+    );
+
+    this.DescriptionClick = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[2]/textarea',
+    );
+
+    this.ClickonMedium = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[3]/div/button[2]',
+    );
+
+    this.RaiseFunding = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[6]/button/svg/path',
+    );
+    this.SubmitButton = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[5]/button[3]',
+    );
+
+    this.TableSearch = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[1]/div[1]/div[1]/input',
+    );
+
+    this.send_for_Approval = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[1]/td[2]/div[1]',
+    );
+
+    this.send = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[5]/button[4]',
+    );
+
+    this.Select_foreman_Option = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[7]/div/div[1]/div[1]/select',
+    );
+
+    // enter the description for this window
+    let DesVar2 = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[7]/div/div[1]/div[2]/textarea',
+    );
+
+    // click on send for Approval.
+    this.SendApproval = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[7]/div/div[2]/button[2]',
+    );
+
+    this.Search = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[1]/div[1]/div[1]/input',
+    );
+    this.SCHButton = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[2]/td[2]/div[1]',
+    );
+
+    // Select status in approval IDs and lets do this scenario.
+    this.Btn1 = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[5]/td[2]/div[1]',
+    );
+
+    // Click on the button Approve and close
+    this.ApprvandClose = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[5]/button[2]',
+    );
+
+    this.AutoMFunctSearchBar = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[1]/div[1]/div[1]/input',
+    );
+
+    this.NewBttn = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[1]/td[2]/div[1]',
+    );
+
+    // click on settings
+    this.Settings = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/a[4]/span',
+    );
+
+    //click on Dashboard
+    this.Dashboard = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/a[1]/span',
+    );
+
+    // Click on findings
+
+    this.Findings = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/a[3]/span',
+    );
+
+    // click on Chcklist
+    this.Checklist = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/div/button/div/span',
+    );
   }
 
   // ========== ACTIONS ==========
@@ -30,6 +133,74 @@ class smartchecklist_12 {
     await this.CDUClick.click();
     await this.RoundChecklistoptionClick.click();
     await this.OutsidereadingOptionClick.click();
+  }
+
+  async ReportFindingSteps() {
+    await this.ClickonOPS_CDU_0057.click();
+    await this.ClickonReportFinding.click();
+    await this.ClickonDropdown.click();
+    await this.page.keyboard.press("ArrowDown");
+    await this.page.keyboard.press("Enter");
+  }
+
+  async NewFindingsWindow() {
+    await this.DescriptionClick.click();
+    await this.DescriptionClick.fill("Hello");
+    await this.ClickonMedium.click();
+    await this.RaiseFunding.click();
+    await this.SubmitButton.click();
+  }
+
+  async ShiftSupervisorFunctions() {
+    await this.TableSearch.hover();
+    await this.TableSearch.fill("OPS-CDU-3.2-0053");
+    await this.TableSearch.press("Enter");
+    await this.send_for_Approval.click();
+    await this.send.click();
+    await this.Select_foreman_Option.click();
+
+    // Lets select the option
+    await this.page.keyboard.press("ArrowDown");
+    await this.page.keyboard.press("Enter");
+
+    await this.DesVar2.fill("Testin/Approval");
+    await this.SendApproval.click();
+
+    //javascript dialog box click ok
+    await this.page.once("dialog", async (dialog) => {
+      await this.dialog.accept(); // Clicks OK
+    });
+  }
+
+  //Foreman Functionalities Method.
+  async AutomationOperationFormanFunctionalities() {
+    await this.Search.fill("ops-cdu-3.2-0001");
+    await this.locator.press("Enter");
+    await this.SCHButton.click();
+    await this.Btn1.click();
+    await this.ApprvandClose.click();
+
+    //close the popup by clicking ok
+    await this.page.once("dialog", async (dialog) => {
+      await this.dialog.accept(); // Clicks OK
+    });
+  }
+
+  async AutomtionOperatorUserFunctionality() {
+    await this.AutoMFunctSearchBar.fill("ops-cdu-3.2-0001");
+    await this.AutoMFunctSearchBar.press("Enter");
+    await this.NewBttn.click();
+  }
+
+  async SettngsDashboardFunctionsLastSection() {
+    await this.Settings.click();
+    await this.Dashboard.click();
+    await this.Findings.clcik();
+    await this.Dashboard.click();
+    await this.Settings.click();
+    await this.Checklist.click();
+    await this.Dashboard.click();
+    await this.Dashboard.click();
   }
 }
 
--- tests/smartchecklist_12.spec.js
+++ tests/smartchecklist_12.spec.js
@@ -2,11 +2,10 @@ const { test, expect } = require("@playwright/test");
 const LoginPage = require("../Pages/LoginPage");
 const smartchecklist_12 = require("../Pages/smartchecklist_12");
 
-test("Validate the End-End Flow for Scenario_3 smartchecklist_12 from Botgauge Scenario is working or not ? ", async ({
+test("Validate the Automation User Basic Login and  Logout is Working? ", async ({
   page,
 }) => {
   const loginpage = new LoginPage(page);
-  const SmartChecklist12 = new smartchecklist_12(page);
   // Open URL
   await loginpage.goto("http://4.213.96.168:5713/");
 
@@ -18,9 +17,14 @@ test("Validate the End-End Flow for Scenario_3 smartchecklist_12 from Botgauge S
 
   // Now logout from Automation User
   await loginpage.logout();
-
-  //------------------------- 1 st part Completed log out from Automation User ----------------------------------//
-
+});
+//------------------------- 1 st part Completed log out from Automation User ----------------------------------//
+test("Validate the Automation Field Operator Basic Login and  Logout is Working? ", async ({
+  page,
+}) => {
+  const loginpage = new LoginPage(page);
+  // Open URL
+  await loginpage.goto("http://4.213.96.168:5713/");
   // Server Configuration
   await loginpage.configureServer("52.140.56.11");
 
@@ -29,9 +33,15 @@ test("Validate the End-End Flow for Scenario_3 smartchecklist_12 from Botgauge S
 
   // Now logout from Automation Field Operator User
   await loginpage.logout();
-
-  //----------------------2 nd part completed log out from Automation Field Operator User------------------//
-
+});
+//----------------------2 nd part completed log out from Automation Field Operator User------------------//
+test("Validate the Shift Supervisor User with chceklist,Operations,CDU Functions is Working? ", async ({
+  page,
+}) => {
+  const loginpage = new LoginPage(page);
+  const SmartChecklist123 = new smartchecklist_12(page);
+  // Open URL
+  await loginpage.goto("http://4.213.96.168:5713/");
   // Server Configuration
   await loginpage.configureServer("52.140.56.11");
 
@@ -39,10 +49,143 @@ test("Validate the End-End Flow for Scenario_3 smartchecklist_12 from Botgauge S
   await loginpage.login("Automation", "Automation2026");
 
   // Do the Functionalities from Shift Supervisor like--- chceklist,Operations,CDu etc.
-  await SmartChecklist12.AutomationUserFunctions();
+  await SmartChecklist123.AutomationUserFunctions();
 
   // Now logout from Automation User
   await loginpage.logout();
-
-  //------------------------3rd Part Completed Log out from Shift Supervisor User---------------------------//
 });
+//------------------------3rd Part Completed Log out from Shift Supervisor User---------------------------//
+test("Validate the Field Operator User with Report Findins Functions is Working as Expeced or Not? ", async ({
+  page,
+}) => {
+  const loginPages = new LoginPage(page);
+  const SmartChecklist123 = new smartchecklist_12(page);
+  // Open URL
+  await loginPages.goto("http://4.213.96.168:5713/");
+  // Server Configuration
+  await loginPages.configureServer("52.140.56.11");
+
+  // Login as  Field Operator User
+  await loginPages.login("AutomationFieldOperator", "Automation2026");
+
+  // need to fix the errors for two methods below----$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  //click on the OPS-CDU-3.2-0057 ID and do the other functionalities like report finding etc.
+  await SmartChecklist123.ReportFindingSteps();
+
+  //create New Findings Functionalities.
+  await SmartChecklist123.NewFindingsWindow();
+
+  //-----------------$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ need to fix between this codes.
+
+  // Now logout from Automation User
+  await loginPages.logout();
+
+  //-------------------4th Session Completed coding need to fix issue here---------------------------------------------
+});
+
+test("Validate the Shift SupervisorUser is Working as Expeced or Not? ", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  // Open URL
+  await loginPage.goto("http://4.213.96.168:5713/");
+
+  // Server Configuration
+  await loginPage.configureServer("52.140.56.11");
+
+  // Login as Automation User
+  await loginPage.login("Automation", "Automation2026");
+
+  // Now logout from Automation User
+  await loginPage.logout();
+});
+//=================================5th session completed==============================================================
+test("Validate the Automation Operator Foreman is Working as Expeced or Not? ", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  // Open URL
+  await loginPage.goto("http://4.213.96.168:5713/");
+
+  // Server Configuration
+  await loginPage.configureServer("52.140.56.11");
+
+  // Login as Automation User
+  await loginPage.login("AutomationOperationForeman", "Automation2026");
+
+  // Now logout from Automation User
+  await loginPage.logout();
+});
+//--------------------------------6th Session Completed---------------------------------------------------------
+
+test("Validate the Shift Supervisor is Working as Expeced or Not? ", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  const SmartChecklist123 = new smartchecklist_12(page);
+  // Open URL
+  await loginPage.goto("http://4.213.96.168:5713/");
+
+  // Server Configuration
+  await loginPage.configureServer("52.140.56.11");
+
+  // Login as Automation User
+  await loginPage.login("Automation", "Automation2026");
+
+  //Shift Supervisor Functioanlities in this Method.
+  await SmartChecklist123.ShiftSupervisorFunctions();
+
+
+  //------------------$$$$$$$$$$$$$$$$$$$$$$$$$$ Here Small 3 to 5 lines code is missing i Guess need to do Final Review over here
+  //to confirm its complete or no---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4
+
+ // Now logout from Shift Supervisor User
+  await loginPage.logout();
+});
+//----------------------------------------------------------7th completed------------------------------------------
+
+test("Validate the Automation Operator Foreman with Functionalities  is Working..!!!! ", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+  const SmartChecklist123 = new smartchecklist_12(page);
+  // Open URL
+  await loginPage.goto("http://4.213.96.168:5713/");
+
+  // Server Configuration
+  await loginPage.configureServer("52.140.56.11");
+
+  // Login as Automation User
+  await loginPage.login("AutomationOperationForeman", "Automation2026");
+
+  //Automation Operator foreman Functioanlities
+  await SmartChecklist123.AutomationOperationFormanFunctionalities();
+
+  //&&&&&&&&&&&&&&&&& Need to Add 3 Scenarios Here and then this Block Is completed----$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+  // automation  Operator Foremen Log out Function
+  await loginPage.logout();
+
+});
+//--------------------------------------------8th completed------------------------------------------------------------
+test("Validate the Automation Field Operator User with other Final functions is Working or Not? ", async ({
+  page,
+}) => {
+  const loginPages = new LoginPage(page);
+  const SmartChecklist123 = new smartchecklist_12(page);
+  // Open URL
+  await loginPages.goto("http://4.213.96.168:5713/");
+  // Server Configuration
+  await loginPages.configureServer("52.140.56.11");
+
+  // Login as  Field Operator User
+  await loginPages.login("AutomationFieldOperator", "Automation2026");
+
+  // Check the Other Functionalities last sessionof the Scenario 3---------
+  await SmartChecklist123.SettngsDashboardFunctionsLastSection();
+
+  //Final Logout for Automation Field Operator
+  await loginPages.logout();
+
+});
+//---------------------------------Completed Last Session Scenario ------------------------------------------
