8c22d1ffbb9638ee71de1dcf8c0d8e02c004d5ae
mohammed hashir kp <hashirthalassery98@gmail.com>
2026-03-03 18:30:16 +0530

    scenario_0001 added for Aramco

--- tests/Pages/GroupPage.js
+++ tests/Pages/GroupPage.js
@@ -9,18 +9,18 @@ class GroupPage {
     this.OperationMenu = page.locator(
       '//*[@id="root"]/div/div/aside/nav/div/div/div[1]/button/span',
     );
-    this.subGroupButton = page.locator(
+    this.CDUClick = page.locator(
       '//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/button/span',
     );
-    this.NextOptButton = page.locator(
-      '//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/div/div[3]/button/span',
+    this.NextOptButton2_0 = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/div/div[2]/button/span',
     );
-    this.NextButton = page.locator(
-      '//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/div/div[3]/div/button[2]/span[2]',
+    this.NextButton2_5 = page.locator(
+      'xpath=//*[@id="root"]/div/div/aside/nav/div/div/div[1]/div/div[1]/div/div[2]/div/button[5]/span[2]',
     );
 
-    this.createButton = page.locator(
-      '//*[@id="root"]/div/div/div/main/div/div[2]/button',
+    this.AssignChecklistButton = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/button/span',
     );
 
     this.dropdown = page.locator(
@@ -38,7 +38,22 @@ class GroupPage {
     );
     this.statusCell = page.locator(
       "xpath=/html/body/div/div/div/div/main/div/div[5]/div[2]/table/tbody/tr[1]/td[16]/span",
-      
+    );
+
+    this.IDStatus = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[1]/div[2]/div/table[1]/tbody/tr[6]/td[2]/div/span',
+    );
+
+    this.ChecklistName = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[1]/div[2]/div/table[1]/tbody/tr[5]/td[2]/input',
+    );
+
+    this.AuditorName = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[1]/div[2]/div/table[2]/tbody/tr[1]/td[2]',
+    );
+
+    this.Category = page.locator(
+      'xpath=//*[@id="root"]/div/div/div/main/div/div[2]/div[1]/div[2]/div/table[2]/tbody/tr[3]/td[2]',
     );
   }
 
@@ -47,28 +62,41 @@ class GroupPage {
     await this.ChecklistButton.click();
   }
 
-  
   async navigateToGroup() {
-  
     await this.OperationMenu.click();
-    await this.subGroupButton.click();
-    
+    await this.CDUClick.click();
   }
 
-  async createAssignment() {
-    await this.NextOptButton.click();
-    await this.NextButton.click();
-     await this.createButton.click();
-    await this.dropdown.click();
-    await this.page.keyboard.press("ArrowDown");
-    await this.page.keyboard.press("Enter");
-    await this.saveButton.click();
-  }
+async createAssignment(expect) {
+
+  await this.NextOptButton2_0.click();
+  await this.NextButton2_5.click();
+  await this.AssignChecklistButton.click();
+
+  await expect(this.dropdown).toBeVisible();
+
+  await this.dropdown.click();
+
+await this.page.getByRole('option', {
+  name: 'Automation Field Operator (Field Operator)'
+}).click();
+
+  await this.saveButton.click();
+}
 
   async verifyAssignment(expect) {
-    await expect(this.nameCell).toHaveText("AutomationFieldOperator");
+    await expect(this.nameCell).toHaveText("Automation Field Operator");
     await expect(this.adminCell).toHaveText("Automation");
     await expect(this.statusCell).toHaveText("Assigned");
+  }
+
+  async VerifyIDColumns(expect) {
+    await expect(this.IDStatus).toHaveText("Assigned");
+    await expect(this.ChecklistName).toHaveValue(
+  "2.5 PostIndicatorsValvePIVChecklist"
+);
+    await expect(this.AuditorName).toHaveText("Automation Testing");
+    await expect(this.Category).toHaveText("Operations");
   }
 }
 
---
+++ tests/Spec/Shift_Supervisor_Scenario_001.spec.js
@@ -0,0 +1,55 @@
+const { test, expect } = require("@playwright/test");
+const LoginPage = require("../Pages/LoginPage");
+const GroupPage = require("../Pages/GroupPage");
+
+test("End-End Testing For Shift SupervisorFlow", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const groupPage = new GroupPage(page);
+
+  // Open URL
+  await loginPage.goto("http://4.213.96.168:5713/");
+
+  // Server Config
+  await loginPage.configureServer("52.140.56.11");
+  // Login as Admin
+  await loginPage.login("Automation", "Automation2026");
+
+  //Navigate to Checklist
+  await groupPage.navigateToChecklist();
+
+  //Navigate Further to Create AssignCheklist
+  await groupPage.navigateToGroup();
+
+  // Create the Assignment and Submit
+  await groupPage.createAssignment(expect);
+
+  // wait for dashboard table data
+  await expect(
+    page.getByRole("button", { name: "View" }).first(),
+  ).toBeVisible();
+  const firstRow = page
+    .locator("table tbody tr")
+    .filter({ has: page.getByRole("button", { name: "View" }) })
+    .first();
+
+  // Verify dynamic ID format
+  await expect(firstRow.locator("td").nth(1).locator("div").first()).toHaveText(
+    /^OPS-CDU-2\.5-\d+$/,
+  );
+
+  // Verify the 4 columns
+  //verify the other columns in Dashboard
+  await expect(firstRow.locator("td").nth(2)).toHaveText(
+    "2.5 PostIndicatorsValvePIVChecklist",
+  );
+  await expect(firstRow.locator("td").nth(3)).toHaveText(
+    "Automation Field Operator",
+  );
+  await expect(firstRow.locator("td").nth(4)).toHaveText("—");
+  await expect(firstRow.locator("td").nth(15)).toHaveText("Assigned");
+  //Verify the ID format and other column Values inside the Latest Record ID.
+  await firstRow.getByText(/^OPS-CDU-2\.5-\d+$/).click();
+
+  //Call last function to Verify columns from the Latest ID.
+  await groupPage.VerifyIDColumns(expect);
+});
